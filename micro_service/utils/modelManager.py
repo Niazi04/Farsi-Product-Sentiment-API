d6ba668a6f86819c9e063598c8a2ebb2ce05aec0
Nasty_Hedgeh0g <niazimohammada@gmail.com>
2026-02-10 21:14:56 +0330

    Handled model path problem when running docker container

--- micro_service/utils/modelManager.py
+++ micro_service/utils/modelManager.py
@@ -1,15 +1,19 @@
 import joblib
 from flask import current_app
 from time import sleep
+from pathlib import Path
 
 def loadModelWithRetry() -> bool:
 
     if current_app.config['MODEL_READY']: return True
 
+    BASE_PATH  = Path(__file__).resolve().parents[2]
+    MODEL_PATH = BASE_PATH / "model"
+
     for attempt in range(1, current_app.config['MAX_LOAD_ATTEMPT'] +1):
         try:
-            current_app.config['vectorizer']   = joblib.load("./model/vectorizer.pkl")
-            current_app.config['svm']          = joblib.load("./model/svm_model.pkl")
+            current_app.config['vectorizer']   = joblib.load(MODEL_PATH / "vectorizer.pkl")
+            current_app.config['svm']          = joblib.load(MODEL_PATH / "svm_model.pkl")
             current_app.config['MODEL_READY']  = True
 
             return True
